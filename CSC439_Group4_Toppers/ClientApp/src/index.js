4efb759ef39e70cf16d7891fd9890502c24f1f2f
RyanHuffman <huffmanr@mymail.nku.edu>
2020-03-24 16:37:33 -0400

    Worked on some styling, added a new fonts folder, still working on Contact.js

--- CSC439_Group4_Toppers/ClientApp/src/index.js
+++ CSC439_Group4_Toppers/ClientApp/src/index.js
@@ -4,9 +4,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import '../src/fonts/Lobster-Regular.ttf'
+import WebFont from 'webfontloader';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
+
+WebFont.load({
+    google: {
+        families: ['Lobster']
+    }
+});
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
