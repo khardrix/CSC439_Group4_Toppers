f41b09fc1cdf8cb5bb5e5d4d1ec65a594f6b1c01
RyanHuffman <huffmanr@mymail.nku.edu>
2020-03-25 18:49:46 -0400

    Updated Contact page, further developed the form

--- CSC439_Group4_Toppers/ClientApp/src/pages/Contact.js
+++ CSC439_Group4_Toppers/ClientApp/src/pages/Contact.js
@@ -45,12 +45,16 @@ export class Contact extends Component {
 
                 <h2>CONTACT US</h2>
                 <br />
+                <p>When it comes to food and service, we strive to be the best. So we'd love to hear about your experience, 
+                    your food and your plans for later. <br />Message us below.
+                </p>
+
                 <div id="contactPageUnderHeader">
                     <div id="frmContainer">
                         <form id="frmContact">
-                            <div class="frmContactContactReason">
-                                <label>*Reason For Contact:</label>
-                                <select id="frmContactReason"
+                            <div class="frmContactContactReasonSelectDiv">
+                                <label for="contactReason">*Reason For Contact:</label>
+                                <select
                                     name="contactReason"
                                     placeholder="Please select the reason you are contacting us today from the drop down list"
                                     value={this.state.contactReason}
@@ -80,118 +84,145 @@ export class Contact extends Component {
                                 </select>
                             </div>
                             <br />
-                            <input
-                                name="fullName"
-                                placeholder="Full Name (example: John Smith)"
-                                value={this.state.fullName}
-                                onChange={e => this.change(e)}
-                            />
-                            <br />
-                            <input
-                                name="email"
-                                placeholder="Email (example: johnsmith@example.com)"
-                                value={this.state.email}
-                                onChange={e => this.change(e)}
-                            />
-                            <input
-                                name="phoneNumber"
-                                placeholder="Phone Number (don't format number. example: 123456789)"
-                                value={this.state.phoneNumber}
-                                onChange={e => this.change(e)}
-                            />
-                            <br />
-                            <input
-                                name="address"
-                                placeholder="Street Address (example: 1234 Main Street)"
-                                value={this.state.address}
-                                onChange={e => this.change(e)}
-                            />
-                            <input
-                                name="aptNumber"
-                                placeholder="Apartment Number (*Leave blank if does not apply* / example: 3A)"
-                                value={this.state.aptNumber}
-                                onChange={e => this.change(e)}
-                            />
-                            <br />
-                            <input
-                                name="city"
-                                placeholder="City (example: Cincinnati)"
-                                value={this.state.city}
-                                onChange={e => this.change(e)}
-                            />
-                            <div id="frmContactStateSelect">
-                                <label>State:</label>
-                                <select id="frmUSState"
-                                    name="usState"
-                                    placeholder="State"
-                                    value={this.state.usState}
+                            <div id="frmContactFullNameDiv">
+                                <label for="fullName">*Full Name:</label>
+                                <input
+                                    id="frmContactFullNameInput"
+                                    name="fullName"
+                                    placeholder="example: John Smith"
+                                    value={this.state.fullName}
                                     onChange={e => this.change(e)}
-                                >
-                                    <option value="AL">AL</option>
-                                    <option value="AK">AK</option>
-                                    <option value="AZ">AZ</option>
-                                    <option value="AR">AR</option>
-                                    <option value="CA">CA</option>
-                                    <option value="CO">CO</option>
-                                    <option value="CT">CT</option>
-                                    <option value="DE">DE</option>
-                                    <option value="FL">FL</option>
-                                    <option value="GA">GA</option>
-                                    <option value="HI">HI</option>
-                                    <option value="ID">ID</option>
-                                    <option value="IL">IL</option>
-                                    <option value="IN">IN</option>
-                                    <option value="IA">IA</option>
-                                    <option value="KS">KS</option>
-                                    <option value="KY">KY</option>
-                                    <option value="LA">LA</option>
-                                    <option value="ME">ME</option>
-                                    <option value="MD">MD</option>
-                                    <option value="MA">MA</option>
-                                    <option value="MI">MI</option>
-                                    <option value="MN">MN</option>
-                                    <option value="MS">MS</option>
-                                    <option value="MO">MO</option>
-                                    <option value="MT">MT</option>
-                                    <option value="NE">NE</option>
-                                    <option value="ND">ND</option>
-                                    <option value="NH">NH</option>
-                                    <option value="NJ">NJ</option>
-                                    <option value="NM">NM</option>
-                                    <option value="NY">NY</option>
-                                    <option value="NC">NC</option>
-                                    <option value="ND">ND</option>
-                                    <option value="OH">OH</option>
-                                    <option value="OK">OK</option>
-                                    <option value="OR">OR</option>
-                                    <option value="PA">PA</option>
-                                    <option value="RI">RI</option>
-                                    <option value="SC">SC</option>
-                                    <option value="SD">SD</option>
-                                    <option value="TN">TN</option>
-                                    <option value="TX">TX</option>
-                                    <option value="UT">UT</option>
-                                    <option value="VT">VT</option>
-                                    <option value="VA">VA</option>
-                                    <option value="WA">WA</option>
-                                    <option value="WV">WV</option>
-                                    <option value="WI">WI</option>
-                                    <option value="WY">WY</option>
-                                </select>
+                                />
                             </div>
-                            <input
-                                name="zipCode"
-                                placeholder="Zip Code (example: 45030)"
-                                value={this.state.zipCode}
-                                onChange={e => this.change(e)}
-                            />
                             <br />
-                            <textarea id="frmtxtAreaMessage" rows="4" cols="65"
-                                name="message"
-                                placeholder="Message (example: The food was wonderful!)"
-                                value={this.state.message}
-                                onChange={e => this.change(e)}
-                            />
+                            <div id="frmContactEmail">
+                                <label for="email">*Email:</label>
+                                <input
+                                    name="email"
+                                    placeholder="example: johnsmith@example.com"
+                                    value={this.state.email}
+                                    onChange={e => this.change(e)}
+                                />
+                            </div>
+                            <div id="frmContactPhoneNumber">
+                                <label for="phoneNumber">*Phone Number:</label>
+                                <input
+                                    name="phoneNumber"
+                                    placeholder="don't format # - example: 123456789"
+                                    value={this.state.phoneNumber}
+                                    onChange={e => this.change(e)}
+                                />
+                            </div>
+                            <br />
+                            <div id="frmContactAddressInput">
+                                <label for="address">*Address:</label>
+                                <input
+                                    name="address"
+                                    placeholder="example: 1234 Main Street"
+                                    value={this.state.address}
+                                    onChange={e => this.change(e)}
+                                />
+                            </div>
+                            <div id="frmContactAptNumberInput">
+                                <label for="aptNumber">Apt #:</label>
+                                <input
+                                    name="aptNumber"
+                                    placeholder="*If applicable - example: 3A"
+                                    value={this.state.aptNumber}
+                                    onChange={e => this.change(e)}
+                                />
+                            </div>
+                            <br />
+                            <div id="frmContactCityStateZip">
+                                <div id="frmContactCity">
+                                    <label for="city">*City</label>
+                                    <input
+                                        name="city"
+                                        placeholder="example: Cincinnati"
+                                        value={this.state.city}
+                                        onChange={e => this.change(e)}
+                                    />
+                                </div>
+                                <div id="frmContactStateSelect">
+                                    <label for="usState">*State:</label>
+                                    <select id="frmUSState"
+                                        name="usState"
+                                        placeholder="State"
+                                        value={this.state.usState}
+                                        onChange={e => this.change(e)}
+                                    >
+                                        <option value="AL">AL</option>
+                                        <option value="AK">AK</option>
+                                        <option value="AZ">AZ</option>
+                                        <option value="AR">AR</option>
+                                        <option value="CA">CA</option>
+                                        <option value="CO">CO</option>
+                                        <option value="CT">CT</option>
+                                        <option value="DE">DE</option>
+                                        <option value="FL">FL</option>
+                                        <option value="GA">GA</option>
+                                        <option value="HI">HI</option>
+                                        <option value="ID">ID</option>
+                                        <option value="IL">IL</option>
+                                        <option value="IN">IN</option>
+                                        <option value="IA">IA</option>
+                                        <option value="KS">KS</option>
+                                        <option value="KY">KY</option>
+                                        <option value="LA">LA</option>
+                                        <option value="ME">ME</option>
+                                        <option value="MD">MD</option>
+                                        <option value="MA">MA</option>
+                                        <option value="MI">MI</option>
+                                        <option value="MN">MN</option>
+                                        <option value="MS">MS</option>
+                                        <option value="MO">MO</option>
+                                        <option value="MT">MT</option>
+                                        <option value="NE">NE</option>
+                                        <option value="ND">ND</option>
+                                        <option value="NH">NH</option>
+                                        <option value="NJ">NJ</option>
+                                        <option value="NM">NM</option>
+                                        <option value="NY">NY</option>
+                                        <option value="NC">NC</option>
+                                        <option value="ND">ND</option>
+                                        <option value="OH">OH</option>
+                                        <option value="OK">OK</option>
+                                        <option value="OR">OR</option>
+                                        <option value="PA">PA</option>
+                                        <option value="RI">RI</option>
+                                        <option value="SC">SC</option>
+                                        <option value="SD">SD</option>
+                                        <option value="TN">TN</option>
+                                        <option value="TX">TX</option>
+                                        <option value="UT">UT</option>
+                                        <option value="VT">VT</option>
+                                        <option value="VA">VA</option>
+                                        <option value="WA">WA</option>
+                                        <option value="WV">WV</option>
+                                        <option value="WI">WI</option>
+                                        <option value="WY">WY</option>
+                                    </select>
+                                </div>
+                                <div id="frmContactZipCode">
+                                    <label for="zipCode">*Zip Code:</label>
+                                    <input
+                                        name="zipCode"
+                                        placeholder="example: 45030"
+                                        value={this.state.zipCode}
+                                        onChange={e => this.change(e)}
+                                    />
+                                </div>
+                            </div>
+                            <br />
+                            <div id="frmContactTextarea">
+                                <label for="frmtxtAreaMessage">*Message:</label>
+                                <textarea id="frmtxtAreaMessage" rows="4" cols="65"
+                                    name="message"
+                                    placeholder="Message (example: The food was wonderful!)"
+                                    value={this.state.message}
+                                    onChange={e => this.change(e)}
+                                />
+                            </div>
                             <br />
                             <button id="btn_contactUsFormSubmit" onClick={e => this.onSubmit(e)}>Submit</button> 
                         </form>
